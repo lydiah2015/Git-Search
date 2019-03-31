7f33f64545d8212362a8e17518d3cce656247ba5
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-03-31 21:15:44 +0300

    created initial app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
