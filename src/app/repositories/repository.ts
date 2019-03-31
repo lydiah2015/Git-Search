df165e3bba73623a701c264574672e78fcc727d1
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-01 01:07:28 +0300

    created class for the repository

--- src/app/repositories/repository.ts
+++ src/app/repositories/repository.ts
@@ -1,2 +1,11 @@
 export class Repository {
+
+    constructor(
+        public name: string,
+        public url: string,
+        public description: string,
+        public stars: number,
+        public language: string,
+        public forks: number,
+    ){}
 }
