f7eefd848ff3f2408c8c1200bad168182491ed6e
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-01 03:58:01 +0300

    created event function for the repo search

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Repository} from "./repository";
+import {GithubsearchService} from "../services/githubsearch.service"
 
 @Component({
   selector: 'app-repositories',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepositoriesComponent implements OnInit {
 
-  constructor() { }
+  searchRepos:Repository[];
+  searchName:string;
+
+  constructor(public githubService:GithubsearchService) {
+    this.searchRepos=[];
+   }
 
   ngOnInit() {
+  }
+
+  searchRepo(){
+    this.githubService.searchRepos(this.searchName).then(()=>{
+      this.searchRepos=this.githubService.searchedRepos;
+    }).catch(error=>{
+      console.log(error)
+    })
   }
 
 }
