9aefb6aea284191517c71323516883588e922b13
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-01 03:54:42 +0300

    fixed get user repos interface bug

--- src/app/services/githubsearch.service.ts
+++ src/app/services/githubsearch.service.ts
@@ -1,27 +1,16 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment"
 import {HttpClient} from "@angular/common/http";
+import {Profile} from "../profile/profile";
+import {Repository} from "../repositories/repository"
 
-
-interface  ProfileInterface{
-  login: string;
-  avatar_url: string;
-  html_url: string;
-  name: string;
-  email: string;
-  public_repos: number;
-  followers: number;
-  following: number;
-}
-
-
-export interface RepositoriesInterface {
+interface RepositoriesInterface {
   total_count: number;
   incomplete_results: boolean;
-  items: Item[];
+  items: Repo[];
 }
 
-interface Item {
+interface Repo {
   name: string;
   full_name: string;
   html_url: string;
@@ -38,16 +27,34 @@ export class GithubsearchService {
 
   GitHubApiKey:string=environment.GitHubApiKey;
 
+  profile:Profile;
+  profileRepos:Repository[];
+  searchedRepos:Repository[];
+
   constructor(private http:HttpClient) { 
+    this.profileRepos=[];
+    this.searchedRepos=[];
 
   }
 
-
   getUser(username:string){
+
+    interface  ProfileInterface{
+      login: string;
+      avatar_url: string;
+      html_url: string;
+      name: string;
+      email: string;
+      public_repos: number;
+      followers: number;
+      following: number;
+    }
+
     let promise=new Promise((resolve,reject)=>{
-      this.http.get<ProfileInterface>("https://api.github.com/users/"+username+"?access_token="+this.GitHubApiKey).toPromise().then(
+      let url="https://api.github.com/users/"+username+"?access_token="+this.GitHubApiKey;
+      this.http.get<ProfileInterface>(url).toPromise().then(
         res=>{
-          console.log(res)
+          this.profile= new Profile(res.login,res.avatar_url,res.html_url,res.name,res.email,res.followers,res.following)
           resolve()
         },
         error=>{
@@ -57,18 +64,28 @@ export class GithubsearchService {
       )
     })
     return promise;
-
   }
 
   getUserRepos(username:string){
     let promise=new Promise((resolve,reject)=>{
-      this.http.get<RepositoriesInterface>("https://api.github.com/users/"+username+"/repos?access_token="+this.GitHubApiKey).toPromise().then(
+      let url="https://api.github.com/users/"+username+"/repos?access_token="+this.GitHubApiKey;
+      this.http.get<Repo[]>(url).toPromise().then(
         res=>{
-          console.log(res)
+          this.profileRepos=[];
+          if(res.length>0){
+            for(var i=0; i<res.length;i++){
+              this.profileRepos.push(
+                new Repository(res[i].name,res[i].html_url,res[i].description,res[i].stargazers_count,
+                  res[i].language,res[i].forks
+                  )
+              )
+            }
+          }
           resolve()
         },
         error=>{
           console.log(error)
+          this.profileRepos=[];
           reject()
         }
       )
@@ -79,14 +96,27 @@ export class GithubsearchService {
 
   searchRepos(query:string){
     let promise=new Promise((resolve,reject)=>{
-      this.http.get<RepositoriesInterface>("https://api.github.com/search/repositories?q="+query+"&access_token="+this.GitHubApiKey)
+      let url="https://api.github.com/search/repositories?q="+query+"&access_token="+this.GitHubApiKey;
+      this.http.get<RepositoriesInterface>(url)
       .toPromise().then(
         res=>{
           console.log(res)
+          this.searchedRepos=[];
+          if(res.total_count>0){
+            for(var i=0; i<res.items.length;i++){
+              console.log(i)
+              this.searchedRepos.push(
+                new Repository(res.items[i].name,res.items[i].html_url,res.items[i].description,res.items[i].stargazers_count,
+                  res.items[i].language,res.items[i].forks
+                  )
+              )
+            }
+          }
           resolve()
         },
         error=>{
           console.log(error)
+          this.searchedRepos=[];
           reject()
         }
       )
